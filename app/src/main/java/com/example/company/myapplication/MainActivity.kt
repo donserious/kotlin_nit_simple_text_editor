a7296b9ce98c6d8601b06d0c3704b9d915aa4c9d
Aleksander Aksamentov <serious106@mail.ru>
2020-02-13 00:15:55 +0300

    #12022020@+подсчетсловсделан

--- app/src/main/java/com/example/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -2,14 +2,44 @@ package com.example.company.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setAll ()
         // Write your code here
 
+
     }
+
+    private fun setAll () {
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var stroka: String = p0.toString().replace("[^A-Za-z0-9_ ]".toRegex(), " ")
+                stroka = stroka.trim()
+                if (stroka.contains(" ")) {
+                    var arrayWord: List<String> = stroka.split(" ")
+                    stats_view.text = arrayWord.size.toString()
+                }
+
+//                val toast = Toast.makeText(applicationContext,arrayWord.size,Toast.LENGTH_SHORT)
+//                toast.show()
+
+            }
+
+        })
+    }
+
+
 }
