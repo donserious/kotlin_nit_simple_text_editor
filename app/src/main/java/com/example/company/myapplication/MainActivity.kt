96ddff3d21e07d9fce2f11ea6c466f1c381fb99f
aksamentov <serious_ang@mail.ru>
2020-02-17 18:40:54 +0300

    17022020@+сохранениеданных - падаем на тесте при русском языке

--- app/src/main/java/com/example/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,30 +15,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setAll ()
+        stats_view.text = "0"
         // Write your code here
 
 
     }
 
-    private fun setAll () {
-        editText.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {}
+    var text: String = "^"
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    private fun setAll () {
+
+        save_button.setOnClickListener{v : View ->
+            text = editText.text.toString()
+            unsaved_changes_view.text = "All changes saved"
+        }
+
+
+        clear_button.setOnClickListener { v: View ->
+            text = ""
+            editText.setText("")
+            stats_view.text = "0"
+        }
+
+
+
+
+
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString() != text )  unsaved_changes_view.text = "Unsaved changes" else unsaved_changes_view.text = "All changes saved"
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (p0.toString() == "") stats_view.text = "0"
                 var stroka: String = p0.toString().replace("[^A-Za-z0-9_ ]".toRegex(), " ")
                 stroka = stroka.trim()
                 if (stroka.contains(" ")) {
                     var arrayWord: List<String> = stroka.split(" ")
                     stats_view.text = arrayWord.size.toString()
                 }
-
-//                val toast = Toast.makeText(applicationContext,arrayWord.size,Toast.LENGTH_SHORT)
-//                toast.show()
-
             }
-
         })
     }
 
